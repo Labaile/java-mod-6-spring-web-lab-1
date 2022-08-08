d8e0f2eb16619afc7c729e697961a735750d041d
Labaile <hello@labaile.com>
2022-08-08 16:33:10 -0400

    added code for controllers files

--- src/main/java/com/example/Access/Camp/controller/ActivitiesController.java
+++ src/main/java/com/example/Access/Camp/controller/ActivitiesController.java
@@ -1,4 +1,46 @@
 package com.example.Access.Camp.controller;
 
+
+import com.example.Access.Camp.model.Activities;
+import com.example.Access.Camp.service.ActivitiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ActivitiesController {
+
+    @Autowired
+    ActivitiesService activitiesService;
+
+    @PostMapping("/activities")
+    public ResponseEntity<Activities> createMember(@Valid @RequestBody Activities member) {
+        Activities newMember = activitiesService.createMember(member);
+        return ResponseEntity.ok(newMember);
+    }
+    @GetMapping("/activities")
+    public List<Activities> readMembers() {
+        return activitiesService.getMembers();
+    }
+
+    @GetMapping("/activities/{memberId}")
+    public Activities readMember(@PathVariable(value = "memberId") Integer id) {
+        return activitiesService.getMember(id);
+    }
+
+    @PutMapping("/activities/{memberId}")
+    public Activities updateMember(@PathVariable(value = "memberId") Integer id, @RequestBody Activities memberData) {
+        return activitiesService.updateMember(id, memberData);
+    }
+
+    @DeleteMapping("/activities/{memberId}")
+    public void deleteMember(@PathVariable(value = "memberId") Integer id) {
+        activitiesService.deleteMember(id);
+    }
+
+
 }
--- src/main/java/com/example/Access/Camp/controller/SignUpController.java
+++ src/main/java/com/example/Access/Camp/controller/SignUpController.java
@@ -1,4 +1,49 @@
 package com.example.Access.Camp.controller;
 
+
+import com.example.Access.Camp.model.Signups;
+import com.example.Access.Camp.service.SignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api")
 public class SignUpController {
+    @Autowired
+    SignUpService signUpService;
+
+    @PostMapping("/signups")
+    public ResponseEntity<Signups> createMember(@Valid @RequestBody Signups member) {
+        Signups newMember = signUpService.createMember(member);
+        return ResponseEntity.ok(newMember);
+    }
+    @GetMapping("/signups")
+    public List<Signups> readMembers() {
+        return signUpService.getMembers();
+    }
+
+    @GetMapping("/signups/{memberId}")
+    public Signups readMember(@PathVariable(value = "memberId") Integer id) {
+        return signUpService.getMember(id);
+    }
+
+    @PutMapping("/signups/{memberId}")
+    public Signups updateMember(@PathVariable(value = "memberId") Integer id, @RequestBody Signups memberData) {
+        return signUpService.updateMember(id, memberData);
+    }
+
+    @DeleteMapping("/signups/{memberId}")
+    public void deleteMember(@PathVariable(value = "memberId") Integer id) {
+        signUpService.deleteMember(id);
+    }
+
+
+
+
 }
